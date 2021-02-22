c38f31393536a29dce41eac01743a9089fdcc5d8
Teo Oliver <teo.liz.oli@gmail.com>
2021-02-22 15:13:32 +0000

    update BaseClass

--- utilities/BaseClass.py
+++ utilities/BaseClass.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 # Base Page Class
 
 @pytest.mark.usefixtures("setup")
@@ -33,10 +34,11 @@ class BaseClass:
 
     # def get_header(self):
 
+    # def login(self):
 
+    # def logout(self):
 
     def verify_link_presence(self, text):
         element = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located(By.LINK_TEXT, text)
         )
-
